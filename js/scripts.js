cff0041fd3df7a53285bb3af205b7caed0ed8f23
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-13 10:00:36 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// scripts.js
+
+$(function(){
+	//this code will execute after the DOM is loaded
+});
+
+var carouselList = $("#carousel ul");
+
+
+function changeSlide() {
+    carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+    
+    function moveFirstSlide () {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
+}
+
+setInterval(changeSlide, 3000) 
+
